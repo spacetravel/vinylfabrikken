2b4e5b365d20b5b5c21b72711508cb8e3a52e402
Vinylfabrikken <spacetravel.no@gmail.com>
2013-04-29 21:25:16 +0200

    fixed paths

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -1,3 +1,5 @@
+#encoding: utf-8
+
 ActiveAdmin.register_page "Dashboard" do
 
   menu :priority => 1, :label => proc{ I18n.t("active_admin.dashboard") }
@@ -6,6 +8,16 @@ ActiveAdmin.register_page "Dashboard" do
    
     # Here is an example of a simple dashboard with columns and panels.
     #
+
+
+
+    columns do
+      column do
+        panel "Informasjon" do
+           para "Velkommen til Vinylfabrikken administrasjon"
+        end
+      end
+    end
     columns do
       column do
         panel "Nyeste forsideplugger" do
@@ -22,10 +34,51 @@ ActiveAdmin.register_page "Dashboard" do
       end
 
       column do
-        panel "Informasjon" do
-           para "Velkommen til Vinylfabrikken administrasjon"
+        panel "Nyeste sider" do
+          table_for Article.order("created_at desc").limit(5) do
+            column "Overskrift", :title do |news|
+              link_to news.title, edit_admin_artikkel_path(news)
+            end
+            column "Opprettet", :created_at, :sortable => :created_at do |news|
+              (time_ago_in_words news.created_at) + " siden"
+            end
+          end
+          strong { link_to "Se alle sidene", admin_artikkels_path }
         end
       end
+
     end
+
+    columns do
+      column do
+        panel "Nyeste priser" do
+          table_for Price.order("created_at desc").limit(5) do
+            column "Produkt", :name do |news|
+              link_to news.name, edit_admin_pri_path(news)
+            end
+            column "Opprettet", :created_at, :sortable => :created_at do |news|
+              (time_ago_in_words news.created_at) + " siden"
+            end
+          end
+          strong { link_to "Se alle prisene", admin_pris_path }
+        end
+      end
+
+      column do
+        panel "Nyeste spørsmål & svar" do
+          table_for Faq.order("created_at desc").limit(5) do
+            column "Spørsmål", :question do |news|
+              link_to news.question, edit_admin_faq_path(news)
+            end
+            column "Opprettet", :created_at, :sortable => :created_at do |news|
+              (time_ago_in_words news.created_at) + " siden"
+            end
+          end
+          strong { link_to "Se alle spørsmål & svar", admin_faqs_path }
+        end
+      end
+
+    end
+
   end # content
 end
