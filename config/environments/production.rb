657a0590077908f46483c7ab67eb6f44d8db60a0
Vinylfabrikken <spacetravel.no@gmail.com>
2013-04-30 03:49:30 +0200

    added production creds

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,7 +66,13 @@ Vf::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
-
+   Paperclip::Attachment.default_options.merge!(
+      :storage => :s3,
+      :bucket => "vinylfabrikken-dev",
+      :s3_credentials => "#{::Rails.root}/config/s3.yml",
+      :path => "/images/:id/:style/:basename.:extension",
+      :url  => ":s3_eu_url"
+    )
   config.middleware.insert_after(::Rack::Lock, "::Rack::Auth::Basic", "Staging") do |u, p|
     [u, p] == ['vinylfabrikken', 'platepresse']
   end
