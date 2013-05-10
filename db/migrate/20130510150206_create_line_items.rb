8af860211450c47a01850184c968139234c30487
Vinylfabrikken <spacetravel.no@gmail.com>
2013-05-10 17:04:22 +0200

    fixed migration

---
+++ db/migrate/20130510150206_create_line_items.rb
@@ -0,0 +1,11 @@
+class CreateLineItems < ActiveRecord::Migration
+  def change
+    drop_table :line_items
+    create_table :line_items do |t|
+      t.integer :order_id
+      t.integer :cart_id
+
+      t.timestamps
+    end
+  end
+end
