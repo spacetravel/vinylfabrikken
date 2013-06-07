33e12f94093514267bdf1611d0aace46306f1f75
Vinylfabrikken <spacetravel.no@gmail.com>
2013-06-07 16:58:19 +0200

    fix!

---
+++ app/controllers/user_profile_controller.rb
@@ -0,0 +1,21 @@
+class UserProfileController < ApplicationController
+
+	def show
+		@user = User.find(params[:id])
+	end
+
+	def orders
+		@user = User.find(params[:id])
+
+	    if current_user.nil?
+	      logger.debug "hvilken bestilling? " + session[:current_order_id].to_s
+	      @orders = Order.find_all_by_id(session[:current_order_id])
+	    else
+	      @orders = current_user.orders.reverse
+	    end
+
+	    respond_to do |format|
+	      format.html # index.html.erb
+	    end
+	end
+end
